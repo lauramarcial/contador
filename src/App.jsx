bdf7ac3918d42992df1eedf97dd53664f92d15e8
diana laura lopez marcial <l18350593@gmail.com>
2022-05-13 09:35:49 -0500

    final del codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,84 @@
-import { useState } from "react";
+import {useState} from "react";
+function App (){
 
-function App() {
   const [cuenta, setCuenta] = useState(0);
-  const [paso, setPaso] = useState (1);
+  const [paso, setPaso] = useState(1);
 
   const handleMinusClick = () => {
-    console.log(paso);
     setCuenta(cuenta - paso);
   };
-const handleResetClick = () => {
-  setCuenta(0);
-  setPaso(1);
-}
-const handlePlusClick = () => {
-  setCuenta(cuenta + paso);
-}
 
-const handleInputChange = (Event) => {
-  setPaso(Number(e.target.value));
-};
+  const handleResetClick = () => {
+    setCuenta(0);
+    setPaso(1);
+  };
 
+  const handlePlusClick = () => {
+    setCuenta(cuenta + paso);
+  };
 
+  const handleInputChange = (e) => {
 
-return(
-<div className="App">
-  <h3>contador</h3>
-  <hr />
-  <h2 className="text-center">{cuenta}</h2>
-  <hr />
-</div
-style = {{
-  display: "flex",
-  justifyContent: "flex-end",
-  marginRight: "5px",
-}}
->
+    if (isNaN(e.target.value)){
+      return;
+    }
+    setPaso(Number(e.target.value));
+  };
 
+  return (
 
+    <div className="App">
+      <h3> Contador </h3>
+      <hr /> 
+      <h2 className="text-center"> {cuenta} </h2>
+      <hr />
 
+      <div style= {{ display: "flex", 
+        justifyContent:"flex-end",
+        marginRight: "5px",
+        }}> 
 
+        <label>
+          Paso
+          <input 
+          id="paso"
+          type= "text" 
+          onChange = {handleInputChange}
+          value={paso}
+          style= {{marginLeft: "5px", width: "60 px"}}
+          />
+        </label>
+        
+        <button 
+          type = "button" 
+          className ="btn btn-primary"
+          onClick={handleMinusClick}
+          style= {{ marginLeft: "5px"}}
+          >
+            -1
+            
+          </button>
 
+        <button 
+          type = "button" 
+          className ="btn btn-primary"
+          onClick={handleResetClick}
+          style= {{marginLeft: "5px"}}
+          >
+            RESET
+            
+          </button>
 
-
-)
-
-
-
-
-
-
-
+        <button 
+          type = "button" 
+          className ="btn btn-primary"
+          onClick={handlePlusClick}
+          style= {{marginLeft: "5px"}}
+          >+1</button>
+      </div>
+    </div>
+  
+);
 }
 
 export default App;
